3162fa15bcf16056a80bef4d6724ac7c7325d970
Takashi Binns <takashi.binns@icloud.com>
2020-04-03 12:50:53 -0400

    needed to update package.json and rebuild

---
+++ build/precache-manifest.43e70fa4de41d046086f9e8d2940db6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b0a38fdac2a57fceb76ff86145ea19",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49e7ce3194b02ad1b324",
+    "url": "./static/css/2.84f3386a.chunk.css"
+  },
+  {
+    "revision": "175bb7f2863eb3defb56",
+    "url": "./static/css/main.defd461b.chunk.css"
+  },
+  {
+    "revision": "49e7ce3194b02ad1b324",
+    "url": "./static/js/2.2428509e.chunk.js"
+  },
+  {
+    "revision": "e22d610449674e6293cc3507c1274078",
+    "url": "./static/js/2.2428509e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "175bb7f2863eb3defb56",
+    "url": "./static/js/main.e036ca12.chunk.js"
+  },
+  {
+    "revision": "7341ec065c94ba9a9330",
+    "url": "./static/js/runtime-main.80d4be85.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.577f5b4ee512f65dfbcd3d8f1a3193d5.js"
+  "./precache-manifest.43e70fa4de41d046086f9e8d2940db6f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
